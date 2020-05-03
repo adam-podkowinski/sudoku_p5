18bf28a38c9eb4ab055cebf1699e380c70b95627
adampodkowinski <adampodkdev@gmail.com>
2020-05-03 19:48:20 +0200

    Add some new stuff

--- sketch.js
+++ sketch.js
@@ -4,20 +4,20 @@ let sudoku;
 let borderWidth;
 
 function setup() {
-    createCanvas(900, 900);
-    if (res % 3 == 0) {
-        spotSize = width / res - (100 / res);
-    } else {
-        print('hohohohoh you a bad man');
-        return;
-    }
-    borderWidth = 50 / res;
-    sudoku = new Sudoku(.5);
+	createCanvas(900, 900);
+	if (res % 3 == 0) {
+		spotSize = width / res - 100 / res;
+	} else {
+		print('hohohohoh you a bad man');
+		return;
+	}
+	borderWidth = 50 / res;
+	sudoku = new Sudoku(0.5);
 }
 
 function draw() {
-    background(255);
-    if (!sudoku) return;
-    sudoku.show();
-    noLoop();
+	background(255);
+	if (!sudoku) return;
+	sudoku.show();
+	noLoop();
 }
